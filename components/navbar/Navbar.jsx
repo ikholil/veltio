c445bb9e6d2a87c899463b90db10ee72101c07a8
ikholil <ikholil.bd@gmail.com>
2023-01-09 16:48:59 +0600

    error fixed

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { BsChevronDown } from "react-icons/bs";
 import { HiMenu } from "react-icons/hi";
